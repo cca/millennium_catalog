c99a8d07ce94fb090f341dc9174a3d1b0b8e3dac
phette23 <phette23@gmail.com>
2014-09-17 12:27:51 -0700

    add Gruntfile with JSHint, Watch, & SASS tasks

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+  // load all tasks
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    jshint: {
+      options: {
+        browser: true,
+        laxcomma: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      dist: {
+        src: [ 'live/*.js' ]
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'live/styles.css': 'live/styles.scss'
+        },
+        options: {
+          sourcemap: 'none',
+          style: 'compressed',
+          trace: true,
+          update: true
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: 'live/*.scss',
+        tasks: ['sass']
+      },
+      jshint: {
+        files: [
+          'live/*.js'
+          , '!live/jquery.*.js'
+          ],
+        tasks: ['jshint']
+      }
+    }
+  });
+
+  // shortcuts, multi-tasks
+  grunt.registerTask('test', ['jshint']); // @todo scss-lint
+  grunt.registerTask('build', ['sass']); // @todo uglify JS
+  grunt.registerTask('default', ['sass']);
+
+};
