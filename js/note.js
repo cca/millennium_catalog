1f77279938c10b245d20bba70c86ffc2fb6a0700
phette23 <phette23@gmail.com>
2015-04-28 09:17:01 -0700

    clean up note.js code, sane whitespace

--- js/note.js
+++ js/note.js
@@ -1,13 +1,10 @@
 // add emphasis to local note fields in bibdisplay
 var tds = document.getElementsByTagName('TD');
-			for(var k = 0; k < tds.length; k++) {
-				if (tds[k].className=='bibInfoLabel') { 
-					//alert ("bibinfolabel");
-						if (tds[k].innerHTML=="Local note") {
-							var emphasis = "<em>" + tds[k].innerHTML + "</em>";
-							tds[k].innerHTML = emphasis;
-						}
-					
-				}
-			}
-	
+for (var k = 0; k < tds.length; k++) {
+	if (tds[k].className == "bibInfoLabel") {
+		if (tds[k].innerHTML == "Local note") {
+			var emphasis = "<em>" + tds[k].innerHTML + "</em>";
+			tds[k].innerHTML = emphasis;
+		}
+	}
+}
