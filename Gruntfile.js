71167b4393dae5b6ad6220a68e2442ecddca8ae3
phette23 <phette23@gmail.com>
2015-04-28 12:02:08 -0700

    Grunt - reformat, build nml.min.js
    also jshint ignores more files now

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,68 +1,67 @@
 module.exports = function(grunt) {
-  // load all tasks
-  require('load-grunt-tasks')(grunt);
+	// load all tasks
+	require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
-    jshint: {
-      options: {
-        asi: true,
-        browser: true,
-        laxcomma: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      dist: {
-        src: [ 'js/*.js', '!js/*.min.js' ]
-      }
-    },
-    sass: {
-      dist: {
-        files: {
-          'css/styles.css': 'css/styles.scss',
-          'css/styles_s6.css': 'css/styles_s6.scss'
-        },
-        options: {
-          sourcemap: 'none',
-          style: 'compressed',
-          trace: true,
-          update: true
-        }
-      }
-    },
-    uglify: {
-      options: {
-        compress: {
-          drop_console: true
-        }
-      },
-      dist: {
-        files: {
-          'js/videosearch.min.js': 'js/videosearch.js',
-          'js/thesis.min.js': 'js/thesis.js',
-          'js/nml-adv.min.js': 'js/nml-adv.js'
-        }
-      }
-    },
-    watch: {
-      sass: {
-        files: 'css/*.scss',
-        tasks: ['sass']
-      },
-      jshint: {
-        files: [
-          'js/*.js'
-          , '!js/jquery.*.js'
-          ],
-        tasks: ['jshint']
-      }
-    }
-  });
+	grunt.initConfig({
+		jshint: {
+			options: {
+				asi: true,
+				browser: true,
+				laxcomma: true,
+				globals: {}
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			dist: {
+				src: ['js/*.js', '!js/*.min.js', 'js/ga.js', '!js/*.pack.js']
+			}
+		},
+		sass: {
+			dist: {
+				files: {
+					'css/styles.css': 'css/styles.scss',
+					'css/styles_s6.css': 'css/styles_s6.scss'
+				},
+				options: {
+					sourcemap: 'none',
+					style: 'compressed',
+					trace: true,
+					update: true
+				}
+			}
+		},
+		uglify: {
+			options: {
+				compress: {
+					drop_console: true
+				}
+			},
+			dist: {
+				files: {
+					'js/videosearch.min.js': 'js/videosearch.js',
+					'js/thesis.min.js': 'js/thesis.js',
+					'js/nml-adv.min.js': 'js/nml-adv.js',
+					'js/nml.min.js': ['js/nml-images.js', 'js/note.js', 'js/ga.js']
+				}
+			}
+		},
+		watch: {
+			sass: {
+				files: 'css/*.scss',
+				tasks: ['sass']
+			},
+			jshint: {
+				files: [
+					'js/*.js', '!js/jquery.*.js'
+				],
+				tasks: ['jshint']
+			}
+		}
+	});
 
-  // shortcuts, multi-tasks
-  grunt.registerTask('test', ['jshint']); // @todo scss-lint
-  grunt.registerTask('build', ['sass', 'uglify']);
-  grunt.registerTask('default', ['sass']);
-
+	// shortcuts, multi-tasks
+	grunt.registerTask('test', ['jshint']); // @todo scss-lint
+	grunt.registerTask('build', ['sass', 'uglify']);
+	grunt.registerTask('default', ['sass']);
 };
