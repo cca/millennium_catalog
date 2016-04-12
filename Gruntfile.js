bb6db989dd602945a6b50be637ba81bf98cf32ef
phette23 <phette23@gmail.com>
2016-04-12 12:42:02 -0700

    update gruntfile with new Js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,6 +50,8 @@ module.exports = function(grunt) {
 					],
 					'dist/catalog.min.js': [
 						'js/cca-webpac.js'
+						, 'js/title.js'
+						, 'js/citethis.js'
 						, 'js/book-jacket.js'
 						, 'js/fancybox-init.js'
 						, 'js/note.js'
